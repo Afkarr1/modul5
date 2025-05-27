dbe370ddbf6e5c93961275d3de9f7d45f61f5a0b
Sartini <sartini@Rafiafkar-MacBook-Air.local>
2025-05-27 18:23:15 +0700

    Add Product class to models.py

---
+++ service/models.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, name, category, price, available):
+        self.name = name
+        self.category = category
+        self.price = price
+        self.available = available
+
